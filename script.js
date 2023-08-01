060d435251d25a1d29ffedd48e04eff611b9c8b2
iusss <aayushman950@gmail.com>
2023-08-01 20:10:52 +0545

    click number to display on screen

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let displayValue;
+
 let firstNumber, secondNumber, operator;
 
 function add(a, b) { return a + b; }
@@ -26,6 +28,43 @@ function operate (operator, firstNumber, secondNumber) {
             console.log('hmmm')
     }
 }
+
+//clear button
+const clearBtn = document.getElementById("clear");
+clearBtn.addEventListener("click", clear);
+
+function clear() {
+    document.getElementById("screenp").innerHTML = "";
+}
+
+
+// const value = document.getElementsByClassName("hmm")
+// if (value) {
+//     value.addEventListener("click", clickedNumber);
+// }
+
+
+
+// function clickedNumber() {
+//     for (let i = 0; i <= value.length; i++) {
+//         document.getElementById("screenp").innerHTML = value[i].innerHTML;
+//     }
+// }
+
+// var elem = document.getElementsByClassName('hmm');
+// displayValue = elem[i].textContent;
+// for(var i = 0; i < elem.length; i++) {
+//     elem[i].addEventListener('click', function(){
+        
+//         document.getElementById("screenp").innerHTML = `${displayValue}`;
+//     });
+// }
+
+[...document.querySelectorAll('.hmm')].forEach(function(item) {
+    item.addEventListener('click', function() {
+        document.getElementById("screenp").innerHTML = item.innerHTML;
+    });
+});
 
 console.log(add(3,2))
 console.log(subtract(3,2))
