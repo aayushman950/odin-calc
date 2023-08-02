02a3ccf51437b999ec752e8a8444d7e101b5d148
iusss <aayushman950@gmail.com>
2023-08-02 16:36:34 +0545

    can calculate but buggy

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-let displayValue;
-
-let firstNumber, secondNumber, operator;
-
 function add(a, b) { return a + b; }
 
 function subtract(a, b) { return a - b; }
@@ -10,19 +6,19 @@ function multiply(a, b) { return a * b; }
 
 function divide(a, b) { return a / b; }
 
-function operate (operator, firstNumber, secondNumber) {
+function operate (operator, currentValue, secondNumber) {
     switch (operator) {
         case '+':
-            return add(firstNumber, secondNumber)
+            return add(prevValue, currentValue)
             break;
         case '-':
-            return subtract(firstNumber, secondNumber)
+            return subtract (currentValue, secondNumber)
             break;
         case '*':
-            return multiply(firstNumber, secondNumber)
+            return multiply (currentValue, secondNumber)
             break;
         case '/':
-            return divide(firstNumber, secondNumber)
+            return divide (currentValue, secondNumber)
             break;
         default:
             console.log('hmmm')
@@ -34,46 +30,71 @@ const clearBtn = document.getElementById("clear");
 clearBtn.addEventListener("click", clear);
 
 function clear() {
-    document.getElementById("screenp").innerHTML = "";
+    screen.innerHTML = "";
 }
 
-
-// const value = document.getElementsByClassName("hmm")
-// if (value) {
-//     value.addEventListener("click", clickedNumber);
-// }
-
-
-
-// function clickedNumber() {
-//     for (let i = 0; i <= value.length; i++) {
-//         document.getElementById("screenp").innerHTML = value[i].innerHTML;
-//     }
-// }
-
-// var elem = document.getElementsByClassName('hmm');
-// displayValue = elem[i].textContent;
-// for(var i = 0; i < elem.length; i++) {
-//     elem[i].addEventListener('click', function(){
-        
-//         document.getElementById("screenp").innerHTML = `${displayValue}`;
+// //populate
+// function firstDigit() {
+//     [...document.querySelectorAll('.hmm')].forEach(function(item) {
+//         item.addEventListener('click', function() {
+//             displayValue = item.innerHTML;
+//             document.getElementById("screenp").innerHTML = `${displayValue}`;
+//         });
 //     });
 // }
 
-[...document.querySelectorAll('.hmm')].forEach(function(item) {
-    item.addEventListener('click', function() {
-        document.getElementById("screenp").innerHTML = item.innerHTML;
-    });
-});
+// function secondDigit() {
+//     console.log(displayValue)
+// }
 
-console.log(add(3,2))
-console.log(subtract(3,2))
-console.log(multiply(3,2))
-console.log(divide(3,2))
+// firstDigit();
+// secondDigit();
 
-console.log('')
+let currentValue = '';
+let prevValue = '';
+let numbers = document.querySelectorAll('.hmm')
+let screen = document.querySelector('.screen')
+numbers.forEach ((number) => number.addEventListener("click", function(e){
+    handleNumber(e.target.textContent)
+    screen.textContent = currentValue;
+}))
 
-console.log(operate('+',1,2))
-console.log(operate('-',1,2))
-console.log(operate('*',1,2))
-console.log(operate('/',1,2))
+function handleNumber(num) {
+    if  (currentValue < 100) {
+        currentValue += num
+    }
+}
+
+let operator = '';
+let operators = document.querySelectorAll('.op');
+operators.forEach((op) => op.addEventListener("click", function(e){
+    handleOperator(e.target.textContent)
+    screen.textContent = `${prevValue} ${operator}`
+}))
+
+function handleOperator(op) {
+    operator = op;
+    prevValue = currentValue;
+    currentValue = '';
+}
+
+let equals = document.querySelector('.equals')
+equals.addEventListener("click", function(){
+    fff();
+    screen.textContent = prevValue;
+})
+
+function fff(){
+    prevValue = Number(prevValue)
+    currentValue = Number(currentValue)
+
+    if (operator === "+") {
+        prevValue += currentValue;
+    } else if (operator === "-") {
+        prevValue -= currentValue;
+    } else if (operator === "*") {
+        prevValue *= currentValue;
+    } else {
+        prevValue /= currentValue;
+    }
+}
